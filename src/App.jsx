437e86367d6934b8d6b18823d0fe11bc251b20a9
ravvitkim <mirkim6086@naver.com>
2025-09-16 10:40:46 +0900

    커밋

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Modal from './Modal';
 import Title from './Title';
 import Blog from './Blog';
 
-
 function App() {
 
   // 데이터 바인딩
@@ -16,72 +15,72 @@ function App() {
     '자바스터디'
   ]);
 
-
-  const [date, setDate] = useState([
-    '2025-05-01',
-    '2025-06-01',
-    '2025-07-01'
-  ])
+  const [createDate, setCreateDate] = useState([
+    '2025-5-1',
+    '2025-6-1',
+    '2025-7-1',
+  ]);
 
   const [details, setDetails] = useState([
     '심플한 디자인의 코트로 가을에 잘 어울림',
-    '강남 우동의 찐 맛집! 먹어보진않았음',
-    '자바 스터디는 말 만하고 못함'
+    '강남 우동의 찐 맛집! 먹어보진 않았음',
+    '자바 스터디는 말 만하고 못함',
   ])
 
   // 좋아요 누름 숫자를 보관할 스테이트
   const [like, setLike] = useState([0,0,0]);
 
   // 좋아요 처리 함수
-  // function addLikes(){
-  //   setLike(like + 1)
+  // function addLikes(num){
+  //   setLike(like[num]+1)
   // }
 
-
   function changeTitle(){
-    const newTitle = [...title] 
-    newTitle[0] = '여자코트추천'
+    const newTitle = [... title];
+    newTitle[0] = '여자코드추천';
     setTitle(newTitle);
   }
 
-  // 모달 페이지가 보이게/안보이게 작업하기위한 스테이트
+  // 모달페이지가 보이게/안보이게 작업하기위한 스테이트
   const [modal, setModal] = useState(false);
 
-  //직전 선택한 인덱스를 저장할 스테이트
-  const [currentIndex, setCurrentIndex] =useState(null);
+  // 직전 선택한 인덱스를 저장할 스테이트
+  const [currentIndex, setCurrentIndex] = useState(null);
+  
 
-  // 제목 클릭시 모달 보이기
-  function handleTitle(index){
-    // 조건 : 같은 제목을 클릭하면 나타나고 사라짐
-    // 다른 제목을 클릭하면 그냥 보여야 하고.. 
-    if(! modal){
-      //1. 현재 모달이 닫혀있으면 연다.
-      setModal(true);
-      setCurrentIndex(index);
-    } else if(currentIndex == index){
-      //2.같은 타이틀이 선택된 경우
-      setModal(false);
-    }else{
-      setCurrentIndex(index);
-    }
-  }
   return (
     <div className='App'>
+      {/* Title이 위치할 곳 */}
       <Title />
-      {/* 타이틀 정렬하기 */}
-      <Blog
-      title={title} date={date} details={details} like={like} currentIndex={currentIndex} handleTitle={handleTitle}
-      setTitle={setTitle} setLike={setLike}
+
+      {/* <h4 style={{color: 'red', fontSize: '20px'}}>{post}</h4> */}
+
+      {/* Blog 위치할 곳 */}
+      <Blog 
+        title={title}
+        createDate={createDate}
+        details={details}
+        setTitle={setTitle}
+        setCreateDate={setCreateDate}
+        like={like}
+        setLike={setLike}
+        setDetails={setDetails}
+        currentIndex={currentIndex}
+        setCurrentIndex={setCurrentIndex}
+        modal={modal}
+        setModal={setModal}
       />
 
       {/* 상세페이지 나타날 곳 */}
-      {modal ? <Modal color="lightblue" 
-        title={title} date={date} details={details}
-        currentIndex={currentIndex}/>: null }
-
+      {modal ? <Modal 
+          color="lightblue" 
+          title={title} 
+          currentIndex={currentIndex} 
+          createDate={createDate}
+          details={details}
+          />: null }
     </div>
   )
 }
-
 
 export default App
--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,11 +1,13 @@
-//하나의 컴포넌트(조각)
-function Modal({color, title, date, details, currentIndex}){
+// 하나의 컴포넌트(조각)
+// Props Drilling
+function Modal({color, title, currentIndex, createDate, details}){
   return(
-      <div className='modal' style={{backgroundColor:color}}>
+    <div className='modal'
+      style={{background: color}}>
         <h4>{title[currentIndex]}</h4>
-        <p>{date[currentIndex]}</p>
+        <p>{createDate[currentIndex]}</p>
         <p>{details[currentIndex]}</p>
-      </div>
+    </div>  
   )
 }
 
